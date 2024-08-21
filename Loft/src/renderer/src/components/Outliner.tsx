f370eb09d3981c40b143f76bac19d56bdafa96ae
Duskire <duskire@gmail.com>
2024-08-21 14:08:24 +1200

    Added small changes and additions to Quick Settings

--- Loft/src/renderer/src/components/Outliner.tsx
+++ Loft/src/renderer/src/components/Outliner.tsx
@@ -30,71 +30,117 @@ export default function Outliner() {
         style={{ WebkitAppRegion: 'no-drag' }}
       >
         {/* insert drag icon here */}
-        Outliner -
-        <div
-          id="outliner-dropdowns"
-          className="mt-[1.5625rem] fill-current text-main-accent text-xl"
-        >
+        <div className="flex flex-row items-center h-[2.1875rem]">
+          <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-1">
+            <Grip />
+          </div>
+          Outliner -
+        </div>
+        <div id="outliner-dropdowns" className="mt-5 fill-current text-xl">
           {/* connections */}
           <div id="connections" className="">
             <div className="flex flex-row items-center h-[2.1875rem]">
-              <div className="h-[2.1875rem] w-[2.1875rem] mr-2.5">
+              <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
                 <Connections />
               </div>
               Connections
             </div>
-            <ul className="ml-4">
+            <ul className="mx-4">
               <li>test</li>
             </ul>
           </div>
           {/* other people */}
           <div id="other-people" className="">
             <div className="flex flex-row items-center h-[2.1875rem]">
-              <div className="h-[2.1875rem] w-[2.1875rem] mr-2.5">
+              <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
                 <OtherPeople />
               </div>
               Other People
             </div>
-            <ul className="ml-4">
+            <ul className="mx-4">
               <li>test</li>
             </ul>
           </div>
           {/* channels */}
           <div id="channels" className="">
             <div className="flex flex-row items-center h-[2.1875rem]">
-              <div className="h-[2.1875rem] w-[2.1875rem] mr-2.5">
+              <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
                 <Channels />
               </div>
               Channels
             </div>
-            <ul className="ml-4">
+            <ul className="mx-4">
               <li>test</li>
             </ul>
           </div>
           {/* chains */}
           <div id="chains" className="">
             <div className="flex flex-row items-center h-[2.1875rem]">
-              <div className="h-[2.1875rem] w-[2.1875rem] mr-2.5">
+              <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
                 <Chains />
               </div>
               Chains
             </div>
-            <ul className="ml-4">
+            <ul className="mx-4">
               <li>test</li>
             </ul>
           </div>
           {/* quick settings */}
           <div id="quick-settings" className="">
             <div className="flex flex-row items-center h-[2.1875rem]">
-              <div className="h-[2.1875rem] w-[2.1875rem] mr-2.5">
+              <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
                 <Settings />
               </div>
               Quick Settings
             </div>
-            <ul className="ml-4">
-              <li>test</li>
-              <li>test</li>
-              <li>test</li>
+            <ul className="mx-4">
+              {/* mic volume */}
+              <li id="mic-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Mic />
+                </div>
+                test
+              </li>
+              {/* headphone volume */}
+              <li id="headphone-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Headphones />
+                </div>
+                test
+              </li>
+              {/* music volume */}
+              <li id="music-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Music />
+                </div>
+                test
+              </li>
+            </ul>
+            <ul className="flex flex-row mx-4">
+              {/* video togggle */}
+              <li id="mic-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Camera />
+                </div>
+              </li>
+              {/* notification toggle */}
+              <li id="headphone-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Notifications />
+                </div>
+              </li>
+              {/* zen mode */}
+              <li id="music-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Zen />
+                </div>
+              </li>
+              {/* trello integration */}
+              <li id="music-volume" className="flex flex-row items-center h-[2.1875rem]">
+                <div className="fill-current text-main-accent h-[2.1875rem] w-[2.1875rem] mr-2.5">
+                  <Trello />
+                </div>
+              </li>
             </ul>
           </div>
         </div>
